27ce75d4bcd73b91c416706b1874f482e953d4df
Jarne Vercruysse <jarne@vercruysse.dev>
2026-02-06 17:04:34 +0100

    Use direct: prefix for Ghostty Zellij picker command

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -12,7 +12,7 @@
       window-padding-x = 10
       window-padding-y = 10
       confirm-close-surface = false
-      command = shell:~/.local/bin/zellij-picker
+      command = direct:~/.local/bin/zellij-picker
     '';
   };
 
