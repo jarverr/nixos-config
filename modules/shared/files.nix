7a72dcd01f1ce3f75fad48c10bf05ed137d374f5
Jarne Vercruysse <jarne@vercruysse.dev>
2026-02-06 17:30:20 +0100

    Hardcode home directory path in Ghostty command

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -12,7 +12,7 @@
       window-padding-x = 10
       window-padding-y = 10
       confirm-close-surface = false
-      command = ${config.home.homeDirectory}/.local/bin/zellij-picker
+      command = /Users/jarver/.local/bin/zellij-picker
     '';
   };
 
